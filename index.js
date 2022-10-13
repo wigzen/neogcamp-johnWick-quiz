77692c2b0e9f92d7be7fd704f05af438e7c63804
wigzen <61306958+wigzen@users.noreply.github.com>
2022-10-13 21:13:47 +0530

    FIX : Score
    in previous version score not shown, Now user will be able see scores

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
 import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
-import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 import ReadLine from "readline-sync";
 
 
 // Welcome
 function Welcome() {
-    // figlet("WELCOME", {
-    //     horizontalLayout: 'full',
-    //     width: 80,
-    //     whitespaceBreak: true
-    // },function (err, data) {
-    //   if (err) {
-    //     console.log("kaboom");
-    //     return;
-    //   }
-    // chalkAnimation.rainbow(data);
 
-      // setTimeout(() => {
-        let username = ReadLine.question('Tell me your name bro')
-        console.log(chalk.blue.bgGray("Hello")+" "+username)
-    // }, 1000);
-    
-    // });    
+  let username = ReadLine.question('Tell me your name bro')
+  console.log(chalk.blue.bgGray("Hello") + " " + username)
 
-// Play
-
-let QnA = [
-    { 
-        'question':"John Wick is known by a monstrous nickname. What is it?",
-        'ans':"Baba Yaga"
+  let QnA = [
+    {
+      'question': "John Wick is known by a monstrous nickname. What is it?",
+      'ans': "Baba Yaga"
     },
-    { 
-        'question':"The death of John Wick's dog sets off the events of the movie. What was his dog's name?",
-        'ans':"Daisy"
+    {
+      'question': "The death of John Wick's dog sets off the events of the movie. What was his dog's name?",
+      'ans': "Daisy"
     },
-    { 
-        'question':'Part 3 is subtitled "Parabellum" What does the Latin phrase that word comes from mean?',
-        'ans':"If you want peace, prepare for war"
+    {
+      'question': 'Part 3 is subtitled "Parabellum" What does the Latin phrase that word comes from mean?',
+      'ans': "If you want peace, prepare for war"
     },
-    { 
-        'question':"What kind of car do they steal from John in the first movie?",
-        'ans':"1969 Mustang"
+    {
+      'question': "What kind of car do they steal from John in the first movie?",
+      'ans': "1969 Mustang"
     },
-    { 
-        'question':'John Wick has a tattoo that reads "Fortis Fortuna Adiuvat." Where on his body is the tattoo?',
-        'ans':"His Back"
+    {
+      'question': 'John Wick has a tattoo that reads "Fortis Fortuna Adiuvat." Where on his body is the tattoo?',
+      'ans': "His Back"
     }
-]
-let score =0
-    const spinner = createSpinner('Checking Answer')
-    QnA.forEach((val)=>{
-        var ans = ReadLine.question(val.question)
-        if (ans.toUpperCase() == val.ans.toUpperCase()){
-            score+=1
+  ]
+  let score = 0
+  const spinner = createSpinner('Checking Answer')
+  QnA.forEach((val) => {
+    var ans = ReadLine.question(val.question)
+    if (ans.toUpperCase() == val.ans.toUpperCase()) {
+      score += 1
 
-            setTimeout(() => {
-                spinner.success()
-              }, 1000)
-        }
-        else{
-            setTimeout(() => {
-                spinner.error()
-              }, 1000)
-        }
-    })
+      setTimeout(() => {
+        spinner.success()
+      }, 500)
+    }
+    else {
+      setTimeout(() => {
+        spinner.error()
+      }, 500)
+    }
+  })
+  setTimeout(() => {
+    console.log(`${username} your Score is ${score}/5`)
+  }, 1000)
+
 
 }
 
